5c725832b46fd67f702f5e559f5a9ef851f88f36
Mehrin2003 <mehrinafroz401@gmail.com>
2025-01-21 18:36:51 +0600

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,22 @@
+num=[22,44,55,67,89,22]
+#for x in range(2,4):
+#for x in num[2:4]:
+   # print(x)
+    #print(num[x])"""
+
+i=0
+while i<3:
+    #print(i)
+    print(num[i])
+    i+=1
+
+"""i=0
+while i<len(num):
+    print(num[i])
+    i+=1
+    if i==3:
+        break
+
+
+
+"""
